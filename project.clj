9ee770a7f2b1e3dfa45e29e72615c6f8c7200a85
jem <michael.jerger@meissa-gmbh.de>
2019-04-03 20:34:05 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.cli "0.4.1"]
-                 [prismatic/schema "1.1.9"]
-                 [mvxcvi/clj-pgp "0.9.0"]
-                 [dda/dda-config-commons "1.4.1"]
+                 [org.clojure/tools.cli "0.4.2"]
+                 [prismatic/schema "1.1.10"]
+                 [mvxcvi/clj-pgp "0.10.0"]
+                 [dda/dda-config-commons "1.4.2"]
                  [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                 [dda/pallet "0.9.0"]
+                 [dda/pallet "0.9.1"]
                  [keypin "0.7.6"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
@@ -23,10 +23,10 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha2"]
-                    [dda/pallet "0.9.0" :classifier "tests"]
+                   [[org.clojure/test.check "0.10.0-alpha4"]
+                    [dda/pallet "0.9.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
@@ -34,11 +34,11 @@
                     :exclusions [commons-logging]}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.1" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-managed-ide.main
-                       :dependencies [[org.clojure/tools.cli "0.4.1"]]}}
+                       :dependencies [[org.clojure/tools.cli "0.4.2"]]}}
   :classifiers {:tests :test}
   :local-repo-classpath true)
