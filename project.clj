6b90ab754b4ba421ad12f77947f5d5420b8dafee
jem <michael.jerger@meissa-gmbh.de>
2020-07-10 12:04:48 +0200

    Version 1.6.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet-commons "1.6.3"
+(defproject dda/dda-pallet-commons "1.6.4"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
